6f8e9d268e43a5b78ed40bbd59cbca9f78dabbbf
jugaseme <sebastiangarciamedina26@gmail.com>
2025-02-28 22:55:40 -0600

    cambios de links y noticias

---
+++ pages/Noticias/noticias_script.js
@@ -0,0 +1,8 @@
+
+function openSlide() {
+    document.getElementById("slideMenu").style.left = "0"; 
+}
+
+function closeSlide() {
+    document.getElementById("slideMenu").style.left = "-250px"; 
+}
