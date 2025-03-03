697438003a4b95af53d24c2106377160572275f5
jugaseme <sebastiangarciamedina26@gmail.com>
2025-03-03 10:02:36 -0600

    arregle cositas proxima mas responsive o nose

--- pages/servicios/servicios_script.js
+++
@@ -1,7 +0,0 @@
-function openSlide() {
-    document.getElementById("slideMenu").style.left = "0"; 
-}
-
-function closeSlide() {
-    document.getElementById("slideMenu").style.left = "-250px"; 
-}
